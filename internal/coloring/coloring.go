b1b07d62b8851f7984c1deb46edd2d241b7e59c3
vxanthio <basiliki.martios1@gmail.com>
2026-02-05 19:56:55 +0200

    feat(coloring): implement FindPositions logic

--- internal/coloring/coloring.go
+++ internal/coloring/coloring.go
@@ -3,11 +3,28 @@ package coloring
 func FindPositions(text string, substring string) []bool {
 	result := make([]bool, len(text))
 
-	for i := 0; i <= len(text)-1; i++ {
-		if text[i] == substring[0] {
-			result[i] = true
-			break
+	// if substring is empty, nothing to do (we'll handle later)
+	if len(substring) == 0 {
+		return result
+	}
+
+	for i := 0; i <= len(text)-len(substring); i++ {
+
+		match := true // assume match
+
+		for p := 0; p < len(substring); p++ {
+			if text[i+p] != substring[p] {
+				match = false
+				break
+			}
+		}
+
+		if match {
+			for p := 0; p < len(substring); p++ {
+				result[i+p] = true
+			}
 		}
 	}
+
 	return result
 }
