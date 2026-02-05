26742f8ef04c674248a41f660a5c486cf0ab0c3a
vxanthio <basiliki.martios1@gmail.com>
2026-02-05 20:53:03 +0200

    feat(coloring): handle empty substring as full match

--- internal/coloring/coloring.go
+++ internal/coloring/coloring.go
@@ -2,26 +2,29 @@ package coloring
 
 func FindPositions(text string, substring string) []bool {
 	result := make([]bool, len(text))
-
-	// if substring is empty, nothing to do (we'll handle later)
 	if len(substring) == 0 {
-		return result
-	}
+		for i := range result {
 
-	for i := 0; i <= len(text)-len(substring); i++ {
-
-		match := true // assume match
-
-		for p := 0; p < len(substring); p++ {
-			if text[i+p] != substring[p] {
-				match = false
-				break
-			}
+			result[i] = true
 		}
 
-		if match {
+	}
+	if len(substring) != 0 {
+		for i := 0; i <= len(text)-len(substring); i++ {
+
+			match := true // assume match
+
 			for p := 0; p < len(substring); p++ {
-				result[i+p] = true
+				if text[i+p] != substring[p] {
+					match = false
+					break
+				}
+			}
+
+			if match {
+				for p := 0; p < len(substring); p++ {
+					result[i+p] = true
+				}
 			}
 		}
 	}
